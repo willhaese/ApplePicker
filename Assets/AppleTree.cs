d003c20f33596cdf5aac97552195506caa63a529
William Haesemeyer <WRH31@CILABS-119A-21.scilsnet.rutgers.edu>
2017-06-08 19:52:28 -0400

    Updates C code for Apple Tree

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine; 
+using System.Collections; 
 
 public class AppleTree : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Prefab for instantiating apples     
+    public GameObject   applePrefab;
+    // Speed at which the AppleTree moves in meters/second     
+    public float        speed = 1f;
+    // Distance where AppleTree turns around    
+    public float        leftAndRightEdge = 10f;
+    // Chance that the AppleTree will change directions    
+    public float        chanceToChangeDirections = 0.1f;
+    // Rate at which Apples will be instantiated    
+    public float        secondsBetweenAppleDrops = 1f;
+    void Start () {
+        // Dropping apples every second     
+    }     void Update () {
+        // Basic Movement         
+        // Changing Direction     
+    }
 }
