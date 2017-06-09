c5f6fa854dec1fb1de9f6bc542b4f8ebf7176351
William Haesemeyer <WRH31@CILABS-119A-21.scilsnet.rutgers.edu>
2017-06-08 20:18:06 -0400

    Apple C Script Attached
    Apples now disapear after falling off the frame.

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -1,15 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class Apple : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
