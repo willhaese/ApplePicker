e4b92e5934573d4c72b6e1aa910146ff05362006
William Haesemeyer <WRH31@CILABS-119A-21.scilsnet.rutgers.edu>
2017-06-08 20:04:47 -0400

    tree now changes randomly

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -14,11 +14,32 @@ public class AppleTree : MonoBehaviour {
     public float        secondsBetweenAppleDrops = 1f;
     void Start () {
         // Dropping apples every second     
-    }     void Update (){
+    }
+    void Update()
+    {
         // Basic Movement     
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
-        transform.position = pos;     
-        // Changing Direction    
+        transform.position = pos;
+        // Changing Direction     
+        if ( pos.x < -leftAndRightEdge )
+        {
+            speed = Mathf.Abs(speed);
+            // Move right     
+        }
+        else if ( pos.x > leftAndRightEdge )
+        {
+            speed = -Mathf.Abs(speed);
+            // Move left     
+        }
     }
-}
+    void FixedUpdate()
+    {
+        // Changing Direction Randomly    
+        if ( Random.value < chanceToChangeDirections )
+        {         speed *= -1;
+            // Change direction     
+        }
+    }
+
+    }
